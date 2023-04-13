4ffcb280fb32362cb29c2a01bac1324051ef3012
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-04-13 17:13:04 -0500

    Commit

--- src/main/java/src/personas/Estudiante.java
+++ src/main/java/src/personas/Estudiante.java
@@ -5,70 +5,31 @@ import java.util.ArrayList;
 import Calendario.*;
 
 public class Estudiante extends Persona{
-	public String nombre;
-	public int ID_estudiante;
-	public ArrayList<Materia> materias_insctritas;
 	
+	
+	//atributos
 	protected ArrayList<Materia> materias_inscritas;
 	
 	
-	//public Estudiante ( String nombre, int ID_estudiante,  Materia materias_insctritas) {
-		//this.nombre = nombre;
-		//this.ID_estudiante = ID_estudiante;
-		//this.materias_insctritas = materias_insctritas;
-	//}
+	//constructor
 	public Estudiante(String nombre, int ID, String Email){
-		this.nombre = nombre;
-		this.ID_estudiante = ID;
-		this.Email = Email;
-		
+		super (nombre, ID, Email);
 	}
 	
 	
 	//Metodos get y set
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	
-	public int getID() {
-		return ID;
-	}
-	public void setID(int ID) {
-		this.ID = ID;
-	}
-	
-	
-	public String getEmail() {
-		return Email;
-	}
-	public void setEmail(String Email) {
-		this.Email = Email;
-	}
-	
-	
-	//public Materia getMaterias_inscritas() {
-		//return materias_insctritas;
-	//}
-	//public void setMaterias_Insctritas(Materia materias_inscritas) {
-		//this.materias_insctritas = materias_inscritas;
-	//}
-	
 	public ArrayList<Materia> getMaterias_Inscritas(){
-		return materias_insctritas;
+		return materias_inscritas;
 	}
 	public void setMaterias_Inscritas(ArrayList<Materia> materiasInsctritas) {
-		this.materias_insctritas = materiasInsctritas;
+		this.materias_inscritas = materiasInsctritas;
 	}
 	
-	
+	//metodos de la clase
 	public void asignarMateria  (Materia nuevaMateria) {
-		materias_insctritas.add(nuevaMateria);
+		materias_inscritas.add(nuevaMateria);
 	}
 	public void retirarMateria(Materia Materia) {
-		materias_insctritas.remove(Materia);
+		materias_inscritas.remove(Materia);
 	}
 }
