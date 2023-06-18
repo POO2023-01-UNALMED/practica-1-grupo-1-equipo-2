c1eafee52a282ca78e30e56c66f52cc716eced45
JhonEdisonPinto <124928317+JhonEdisonPinto@users.noreply.github.com>
2023-06-18 18:22:16 -0500

    Delete main.py

--- src_py/main.py
+++
@@ -1,9 +0,0 @@
-from personas.Estudiante import Estudiante
-from Calendario.Facultad import Facultad
-from Calendario.Beca import Beca
-from Calendario.Tarea import Tarea
-from Calendario.Materia import Materia
-from personas.Profesor import Profesor
-import tkinter as tk
-
-if __name__ == "__main__":
