92937a54b4ba8e26ed1e518a7705b1733d34323f
JhonEdisonPinto <124928317+JhonEdisonPinto@users.noreply.github.com>
2023-06-18 12:28:39 -0500

    push

--- src_py/TestInterfraz.py
+++ src_py/TestInterfraz.py
@@ -5,20 +5,33 @@ from PIL import Image, ImageTk
 root = Tk()
 root.title("Ventana de Inicio")
 root.geometry("800x600")
+root.configure(background="light blue")
 
 root.grid_rowconfigure(0, weight=1)
 root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 
+# Función para convertir valores RGB a hexadecimal
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+# Convertir los valores RGB a hexadecimal
+color_rgb = (148, 180, 59)
+color_rgb1 =(166,28,49)
+color_fondo =(177,178,176)
+color_hex2 = rgb_to_hex(color_fondo)
+color_hex1 =rgb_to_hex(color_rgb1)
+color_hex = rgb_to_hex(color_rgb)
+
 # Espacios de Frame
-frame = Frame(root)
+frame = Frame(root,background=color_hex,borderwidth=2,highlightthickness=2)
 frame.grid(row=0, column=0, sticky="nsew")
-frame2 = Frame(root)
+frame2 = Frame(root,background=color_hex1,borderwidth=2,highlightthickness=2)
 frame2.grid(row=0, column=1, sticky="nsew")
-frame3 = Frame(root)
+frame3 = Frame(root,background=color_hex1,borderwidth=2,highlightthickness=2)
 frame3.grid(row=1, column=1, sticky="nsew")
-frame4 = Frame(root)
+frame4 = Frame(root,background=color_hex ,borderwidth=2,highlightthickness=2)
 frame4.grid(row=1, column=0, sticky="nsew")
 
 # Configurar la cuadrícula de los frames
@@ -27,37 +40,50 @@ for i in range(2):
     root.grid_columnconfigure(i, weight=1)
 
 # Crear un label para el saludo de bienvenida
-saludo = Label(frame, text="Bienvenido al Sistema de Gestión Universitaria")
+saludo = Label(frame, text="Bienvenido al Sistema de Gestión Universitaria",
+               background="white")
+saludo.config(font=("Times New Roman", 12, "bold"))
 saludo.grid(row=0, column=0, padx=10, pady=10, sticky="n")
 
 # Lista de títulos y textos
-titulos = ["Juan", "Breadley", "Jhon", "Baena"]
+titulos = [" --> Juan <--", " --> Breadley <--", " --> Jhon <--", " --> Baena <--"]
 textos = [
-    """Estudiante de ING de Sistemas de la UNAL
-gustos:
-Virtudes:
-Contacto: Correo@unal.edu.co
+    """-- Estudiante de ING de Sistemas de la UNAL --
+=> gustos:
+=> Virtudes:
+=> Contacto: Correo@unal.edu.co
 """,
-    """Estudiante de C. de la Computación de la UNAL 
-gustos: jugar VideoJuegos, fútbol
-Virtudes: Amable, Atento, Recursivo 
-Contacto: brmarinv@unal.edu.co""",
-    """Estudiante de ING de Sistemas de la UNAL 
-gustos:
-Virtudes:
-Contacto: Correo@unal.edu.co""",
-    """Estudiante de ING de Sistemas de la UNAL 
-gustos:
-Virtudes: 
-Contacto: Correo@unal.edu.co"""
+    """-- Estudiante de C. de la Computación de la UNAL --\n 
+=> Gustos: Resolución de problemas, 
+VideoJuegos competitivos, fútbol 
+y el chocolate en grandes cantidades.\n
+=> Virtudes: Amabilidad, estar atento a 
+las necesidades de los demás, adaptable 
+y con habilidad recursiva.\n 
+=> Contacto: brmarinv@unal.edu.co\n""",
+    """-- Estudiante de ING de Sistemas de la UNAL --\n
+=> Gustos: los retos, ponen a prueba mi 
+capacidad de solucionar problemas 
+y me ayudan a crecer como persona.\n
+=> Virtudes: Soy responsable, comprometido, 
+bueno en la resolución de problemas
+y en la comunicación con el equipo.\n
+=> Contacto: jhpintoh@unal.edu.co\n""",
+    """-- Estudiante de ING de Sistemas de la UNAL --\n
+=> Gustos: aprender nuevas ideas sobre 
+diversas áreas del conocimiento, 
+también proponer soluciones a problemas.\n
+=> Virtudes: facilidad para tomar decisiones,
+y la adaptación rápida a nuevos entornos.\n 
+=> Contacto: jubaenag@unal.edu.co\n"""
 ]
 
 # Lista de imágenes
 imagenes = [
-    ImageTk.PhotoImage(Image.open("src_py/Images/imagen1.png").resize((100, 100))),
-    ImageTk.PhotoImage(Image.open("src_py/Images/imagen2.png").resize((100, 100))),
-    ImageTk.PhotoImage(Image.open("src_py/Images/imagen3.png").resize((100, 100))),
-    ImageTk.PhotoImage(Image.open("src_py/Images/imagen4.png").resize((100, 100))),
+    ImageTk.PhotoImage(Image.open("src_py/Images/imagen1.png").resize((150, 120)).convert("RGBA")),
+    ImageTk.PhotoImage(Image.open("src_py/Images/imagen2.png").resize((150, 120)).convert("RGBA")),
+    ImageTk.PhotoImage(Image.open("src_py/Images/imagen3.png").resize((150, 120)).convert("RGBA")),
+    ImageTk.PhotoImage(Image.open("src_py/Images/imagen4.png").resize((150, 120)).convert("RGBA")),
 ]
 
 # Variables para almacenar el índice actual
@@ -80,11 +106,13 @@ frame2.grid_rowconfigure(1, weight=1)
 frame2.grid_columnconfigure(0, weight=1)
 
 # Centrar los elementos dentro del frame2
-tituloHojaVida = Label(frame2, text=titulos[indice_actual])
-tituloHojaVida.grid(row=0, column=0, padx=10, pady=10, sticky="n")
+tituloHojaVida = Label(frame2, text=titulos[indice_actual], background="white")
+tituloHojaVida.config(font=("Times New Roman", 12, "bold"))
+tituloHojaVida.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-label_texto = Label(frame2, text=textos[indice_actual])
-label_texto.grid(row=1, column=0, padx=10, pady=1, sticky="nsew")
+label_texto = Label(frame2, text=textos[indice_actual], background="white")
+label_texto.config(font=("Times New Roman", 10, "bold"))
+label_texto.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 label_texto.bind("<Button-1>", lambda event: cambiar_texto())
 
 # Imágenes en labels de los desarrolladores
@@ -104,23 +132,23 @@ frame3.grid_columnconfigure(0, weight=1)
 frame3.grid_columnconfigure(3, weight=1)
 
 # Crear imágenes asociadas al sistema
-imagen_sis1 = ImageTk.PhotoImage(Image.open("src_py/Images/sis1.png").resize((100, 100)))
-imagen_sis2 = ImageTk.PhotoImage(Image.open("src_py/Images/sis2.png").resize((100, 100)))
-imagen_sis3 = ImageTk.PhotoImage(Image.open("src_py/Images/sis3.png").resize((100, 100)))
-imagen_sis4 = ImageTk.PhotoImage(Image.open("src_py/Images/sis4.png").resize((100, 100)))
-imagen_sis5 = ImageTk.PhotoImage(Image.open("src_py/Images/sis5.png").resize((100, 100)))
+imagen_sis1 = ImageTk.PhotoImage(Image.open("src_py/Images/sis1.png").resize((100, 120)).convert("RGBA"))
+imagen_sis2 = ImageTk.PhotoImage(Image.open("src_py/Images/sis2.png").resize((100, 120)).convert("RGBA"))
+imagen_sis3 = ImageTk.PhotoImage(Image.open("src_py/Images/sis3.png").resize((100, 120)).convert("RGBA"))
+imagen_sis4 = ImageTk.PhotoImage(Image.open("src_py/Images/sis4.png").resize((100, 120)).convert("RGBA"))
+imagen_sis5 = ImageTk.PhotoImage(Image.open("src_py/Images/sis5.png").resize((100, 120)).convert("RGBA"))
 
 # Imágenes en labels asociadas al sistema
-label_imagen_sis1 = Label(frame4, image=imagen_sis1)
-label_imagen_sis1.grid(row=0, column=1, padx=1, pady=0, sticky="nsew")
-label_imagen_sis2 = Label(frame4, image=imagen_sis2)
-label_imagen_sis2.grid(row=0, column=2, padx=1, pady=0, sticky="nsew")
-label_imagen_sis3 = Label(frame4, image=imagen_sis3)
-label_imagen_sis3.grid(row=0, column=3, padx=1, pady=0, sticky="nsew")
-label_imagen_sis4 = Label(frame4, image=imagen_sis4)
-label_imagen_sis4.grid(row=1, column=1, padx=1, pady=0, sticky="nsew")
+label_imagen_sis1 = Label(frame4, image=imagen_sis1,highlightthickness=2)
+label_imagen_sis1.grid(row=0, column=1, padx=5, pady=10, sticky="nsew")
+label_imagen_sis2 = Label(frame4, image=imagen_sis2,highlightthickness=2)
+label_imagen_sis2.grid(row=0, column=2, padx=5, pady=10, sticky="nsew")
+label_imagen_sis3 = Label(frame4, image=imagen_sis3,highlightthickness=2)
+label_imagen_sis3.grid(row=0, column=3, padx=5, pady=10, sticky="nsew")
+label_imagen_sis4 = Label(frame4, image=imagen_sis4,highlightthickness=2)
+label_imagen_sis4.grid(row=1, column=1, padx=5, pady=10, sticky="nsew")
 label_imagen_sis5 = Label(frame4, image=imagen_sis5)
-label_imagen_sis5.grid(row=1, column=3, padx=1, pady=0, sticky="nsew")
+label_imagen_sis5.grid(row=1, column=3, padx=5, pady=10, sticky="nsew")
 
 # Configurar el peso de las filas y columnas adicionales
 frame4.grid_rowconfigure(0, weight=1)
