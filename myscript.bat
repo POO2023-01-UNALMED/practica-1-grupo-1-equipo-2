739d5833da5196c3ee17e304b25462ec0c4195c1
JhonEdisonPinto <124928317+JhonEdisonPinto@users.noreply.github.com>
2023-06-20 11:19:15 -0500

    Update myscript.bat

--- myscript.bat
+++ myscript.bat
@@ -1,2 +1,2 @@
 @echo off 
-"C:\Users\jhon\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\jhon\Documents\GitHub\practica-1-grupo-1-equipo-2\src_py\main.py" 
+%LOCALAPPDATA%\Programs\Python\Python311\python.exe %USERPROFILE%\Documents\GitHub\practica-1-grupo-1-equipo-2\src_py\main.py 
