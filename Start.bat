27d5b87e2638a9680703b9e6f9baee1b1f81965e
JhonEdisonPinto <124928317+JhonEdisonPinto@users.noreply.github.com>
2023-06-20 10:53:02 -0500

    Intento 3

--- Start.bat
+++ Start.bat
@@ -1,3 +1,3 @@
-@echo off
-"%LOCALAPPDATA%\Programs\Python\Python311\python.exe" %USERPROFILE%\Documents\GitHub\practica-1-grupo-1-equipo-2\src_py\main.py
+@echo off 
+%LOCALAPPDATA%\Programs\Python\Python311\python.exe %USERPROFILE%\Documents\GitHub\practica-1-grupo-1-equipo-2\src_py\main.py 
 pause
